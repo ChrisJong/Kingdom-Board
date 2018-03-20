223514eba82f4ae53a655059774e53fbda81b974
dliang1 <210234X@student.swin.edu.au>
2018-03-20 22:35:25 +1100

    wizard
    +wizard
    +animation testing
    
    =renaming for black white
    =renaming for unit animation clips

---
+++ Assets/_scripts/player/animationTest_dan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationTest_dan : MonoBehaviour {
+
+    public Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("space")) {
+            anim.Play("Attack");
+        }           
+	}
+}
