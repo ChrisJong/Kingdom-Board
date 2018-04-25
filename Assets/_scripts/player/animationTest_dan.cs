cad3f0934bacad37ed5d6cd3111c76e9bb292855
dliang1 <210234X@student.swin.edu.au>
2018-04-26 05:34:57 +1000

    Cleric and Prefab update
    +Cleric Unit
                 -New animation clip naming "Cast"
    +Reworked prefabs for missing materials
    +Added Capsule Colliders
    +Added NavMeshAgents
                 -Have not implemented unit scripts
    +Added new scene to view units in game
                 -Added new camera script for new scene

--- Assets/_scripts/player/animationTest_dan.cs
+++ Assets/_scripts/player/animationTest_dan.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class animationTest_dan : MonoBehaviour {
 
-    public Animator anim;
+    private Animator anim;
 
     private void Start() {
         anim = GetComponent<Animator>();
---
+++ Assets/_scripts/player/cam_dan.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class cam_dan : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 5f;
+    public float scrollSpeed = 10f;
+    private float panLimitX;
+    private Vector2 panLimitY;
+    private float zoomPanLimitRatioX = 10f;
+
+    private float currentZoom = 1f;
+    public float maxZoom = 2.25f;
+    public float minZoom = 0.5f;
+
+    public GameObject mainCamera;
+
+	
+	// Update is called once per frame
+	void Update () {
+        //Detect input to move camera
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || (Input.mousePosition.y >= Screen.height - panBorderThickness)) {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || (Input.mousePosition.y <= panBorderThickness))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || (Input.mousePosition.x <= panBorderThickness))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || (Input.mousePosition.x >= Screen.width - panBorderThickness))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        //Clamps camera to game region
+        pos.x = Mathf.Clamp(pos.x, -panLimitX, panLimitX);
+        pos.z = Mathf.Clamp(pos.z, panLimitY.x, panLimitY.y);
+        //Move camera call function
+        MoveCamera(pos);
+
+        //Detect input to zoom camera
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        //Scroll up means zoom in
+        if (scroll > 0) {
+            currentZoom += scroll;
+            if (currentZoom > maxZoom)
+            {
+                currentZoom = maxZoom;
+            }
+            else {
+                ZoomCamera(scroll);
+            }
+        }
+        //Scroll down means zoom out
+        if (scroll < 0) {
+            currentZoom += scroll;
+            if (currentZoom < minZoom)
+            {
+                currentZoom = minZoom;
+            }
+            else {
+                ZoomCamera(scroll);
+            }
+        }
+
+        //Update camera pan limit to suit zoom level
+        panLimitX = currentZoom * zoomPanLimitRatioX;
+        panLimitY.x = ((5f + (currentZoom * 2.5f)) - (currentZoom * 7.5f));
+        panLimitY.y = ((5f + (currentZoom * 2.5f)) + (currentZoom * 7.5f));
+    }
+
+    void MoveCamera (Vector3 moveDirection) {
+        transform.position = moveDirection;
+    }
+
+    void ZoomCamera (float zoomDirection) {
+        mainCamera.transform.Translate(0, 0, zoomDirection * scrollSpeed);
+    }
+}
