1cdee6e73f64b2f581b9dd81623a8b114bd8dddc
dliang1 <210234X@student.swin.edu.au>
2018-05-12 00:37:46 +1000

    New Map
    +Added new map layout objects
    -Removed old map layout objects
    
    +Redid personal camera script to fit new layout
    +Reorganised textures folder

--- Assets/_scripts/player/cam_dan.cs
+++ Assets/_scripts/player/cam_dan.cs
@@ -7,19 +7,18 @@ public class cam_dan : MonoBehaviour {
     public float scrollSpeed = 10f;
     private float panLimitX;
     private Vector2 panLimitY;
-    private float zoomPanLimitRatioX = 10f;
 
     private float currentZoom = 1f;
-    public float maxZoom = 2.25f;
-    public float minZoom = 0.5f;
+    public float maxZoom;
+    public float minZoom;
 
     public GameObject mainCamera;
 
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {              
         //Detect input to move camera
-        Vector3 pos = transform.position;
+        Vector3 pos = transform.position;        
 
         if (Input.GetKey("w") || (Input.mousePosition.y >= Screen.height - panBorderThickness)) {
             pos.z += panSpeed * Time.deltaTime;
@@ -43,6 +42,7 @@ public class cam_dan : MonoBehaviour {
         //Clamps camera to game region
         pos.x = Mathf.Clamp(pos.x, -panLimitX, panLimitX);
         pos.z = Mathf.Clamp(pos.z, panLimitY.x, panLimitY.y);
+        
         //Move camera call function
         MoveCamera(pos);
 
@@ -59,6 +59,7 @@ public class cam_dan : MonoBehaviour {
                 ZoomCamera(scroll);
             }
         }
+
         //Scroll down means zoom out
         if (scroll < 0) {
             currentZoom += scroll;
@@ -70,11 +71,12 @@ public class cam_dan : MonoBehaviour {
                 ZoomCamera(scroll);
             }
         }
+        Debug.Log(currentZoom);
 
         //Update camera pan limit to suit zoom level
-        panLimitX = currentZoom * zoomPanLimitRatioX;
-        panLimitY.x = ((5f + (currentZoom * 2.5f)) - (currentZoom * 7.5f));
-        panLimitY.y = ((5f + (currentZoom * 2.5f)) + (currentZoom * 7.5f));
+        panLimitX = 8f + (currentZoom * 7f);
+        panLimitY.x = -2.5f - (currentZoom * 5f);
+        panLimitY.y = 10f + (currentZoom * 10f);
     }
 
     void MoveCamera (Vector3 moveDirection) {
