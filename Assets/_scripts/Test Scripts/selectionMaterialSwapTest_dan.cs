ca5b142de7b3c25a4916a2ac1f8253aa08b05bda
dliang1 <210234X@student.swin.edu.au>
2018-05-30 06:51:26 +1000

    New shader for Outlines
    +Added new shader and script for Outlined objects
    +Reorganised temporary scripts under "Test Scripts" folder

---
+++ Assets/_scripts/Test Scripts/selectionMaterialSwapTest_dan.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectionMaterialSwapTest_dan : MonoBehaviour {
+
+    Renderer[] rs;
+
+    private void Start()
+    {
+        rs = GetComponentsInChildren<Renderer>();
+    }
+
+    private void Update()
+    {
+        //Control outline colour
+        //O for green, P for red
+        if (Input.GetKeyDown("o"))
+        {
+            foreach (Renderer r in rs)
+            {
+                r.material.SetColor("_OutlineColor", Color.green);
+            }
+        }
+
+        if (Input.GetKeyDown("p"))
+        {
+            foreach (Renderer r in rs)
+            {
+                r.material.SetColor("_OutlineColor", Color.red);
+            }
+        }
+        //Control show outline
+        //K to hide, L to show
+        if (Input.GetKeyDown("k"))
+        {
+            foreach (Renderer r in rs) {
+                r.material.SetFloat("_Outline", 0f);
+            }
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            foreach (Renderer r in rs)
+            {
+                r.material.SetFloat("_Outline", 0.03f);
+            }
+        }
+    }
+}
