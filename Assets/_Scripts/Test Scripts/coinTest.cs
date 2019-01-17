675324b4e71fc005cce4d002dec42151d29ea665
Daniel-PC\Daniel <rwa.ezimez@gmail.com>
2019-01-18 08:47:59 +1100

    Coin quick update
    +Changed material to support fading in and out
    +Added functionalities in script to do so
    
    --How to use--
    Press space to fade the coin in
    Once it is opaque, it will automatically flip itself
    The coin will fade away once it has landed and after a short delay
    You can flip again once the coin has faded away
    
    +Due to my desire to have the coin on its side in the beginning
    It does not have proper animation transitions which result in
    the coin not flipping if you roll the same result multiple times in a row
    
    This doesn't matter in the long run since these animations are meant to be one shots

--- Assets/_Scripts/Test Scripts/coinTest.cs
+++ Assets/_Scripts/Test Scripts/coinTest.cs
@@ -1,31 +1,121 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class coinTest : MonoBehaviour
 {
-    public GameObject coin;
-    Animator anim;
+    Animator anim;  //animation component of the coin
+    Material mat;   //material component of the coin
 
-    bool atk = true;
+    bool hasBeenFlipped = false;    //quick check to see if this coin has been flipped yet
+
+    bool hasFadedOut = false;   //redundant bool to check if the coin has finished fading out, only use for reset function
+
+    float fadeSpeed = 1f;   //how quickly the coin fades in or out
+    float fadeDelay = 1f;   //delay after the coin lands before we begin to fade out
 
     private void Start()
     {
-        anim = coin.GetComponent<Animator>();
+        //get the components we need to work with
+
+        anim = GetComponent<Animator>();
+        mat = GetComponent<Renderer>().material;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //press space to begin fading in the coin
+
+        if (Input.GetKeyDown(KeyCode.Space) && !hasBeenFlipped)
         {
-            if (atk)
+            StartCoroutine(FadeCoin(true));
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetCoin();
+        }
+    }
+
+    IEnumerator FadeCoin(bool _in)
+    {
+        //Get color of the material on coin
+        Color newColor = mat.color;
+
+        //Wait a bit before fading out, if we're fading out
+        if (!_in)
+            yield return new WaitForSeconds(fadeDelay);
+
+        //Loop to progressively fade in or out
+        if (_in)
+        {
+            while (newColor.a < 1)
             {
-                anim.Play("CoinAttack");
-                atk = false;
-            }
-            else
-            {
-                anim.Play("CoinDefend");
-                atk = true;
+                newColor.a += (fadeSpeed * Time.fixedDeltaTime);
+                if (newColor.a > 1)
+                    newColor.a = 1;
+
+                mat.color = newColor;
+
+                yield return new WaitForFixedUpdate();
             }
         }
+        else
+        {
+            while (newColor.a > 0)
+            {
+                newColor.a -= (fadeSpeed * Time.fixedDeltaTime);
+                if (newColor.a < 0)
+                    newColor.a = 0;
+
+                mat.color = newColor;
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        //If we faded in, then we've finished fading in so flip the coin
+        if (_in)
+            FlipCoin();
+
+        //If we faded out, reset the coin. currently for testing
+        if (!_in)
+            ResetCoin();
+
+        yield return null;
+    }
+
+    void FlipCoin()
+    {
+        //0 - 1 defend and 1.000001 - 2 is attack or whatever floating point
+        //Remove this and use preferred RNG
+        string animationName;
+
+        int rng = UnityEngine.Random.Range(1, 100);
+        Debug.Log(rng);
+
+        if (rng > 51)
+            animationName = "CoinAttack";
+        else
+            animationName = "CoinDefend";
+        anim.Play(animationName);
+    }
+
+    void ResetCoin()
+    {
+        if (!hasFadedOut)
+            return;
+
+        hasBeenFlipped = false;
+    }
+
+    public void OnAnimationComplete()
+    {
+        //callback for when the animation finishes
+        //this script must be placed on the coin itself
+        //for this callback to work
+        //insert extra code here for any callback functionalities required
+
+        StartCoroutine(FadeCoin(false));
     }
 }
