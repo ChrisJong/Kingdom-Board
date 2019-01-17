04cf774a605cef4e3b87e1f01991ed557f092dff
Daniel-PC\Daniel <rwa.ezimez@gmail.com>
2019-01-18 05:19:48 +1100

    Added coin flip test
    +New scene CoinTest ( coinTest.cs [public coin obj, w/ component <Animator>. press space to flip coin])
    
    +Coin model
    +Coin controller
    +Coin animation clips
    +Coin texture
    +Coin material

---
+++ Assets/_Scripts/Test Scripts/coinTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class coinTest : MonoBehaviour
+{
+    public GameObject coin;
+    Animator anim;
+
+    bool atk = true;
+
+    private void Start()
+    {
+        anim = coin.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (atk)
+            {
+                anim.Play("CoinAttack");
+                atk = false;
+            }
+            else
+            {
+                anim.Play("CoinDefend");
+                atk = true;
+            }
+        }
+    }
+}
