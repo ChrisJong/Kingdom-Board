941e8265012ad32c05c96533a5b88a98a7000ee6
Daniel-PC\Daniel <rwa.ezimez@gmail.com>
2019-02-12 14:36:49 +1100

    +Hourglass Update
    -Imported Hourglass Model: HourglassCasing, HourglassGlass, HourglassSandTop, HourglassSandBot
    -Created hourglass prefab under Prefabs/UI/Player
    -New scene to preview the hourglass: hourglass_test.unity
    -New script which drives the hourglass animations: hourglass_Test under _Scripts/Test Scripts
    
    +Reorganised several files into more appropriate folders
    
    -Redid coin animations and moved them under Player/Interface folder for animations, materials, textures
    -Moved XIndicator and line path to same directory
    -Included hourglass into this file path

--- Assets/_Scripts/Test Scripts/coinTest.cs
+++ Assets/_Scripts/Test Scripts/coinTest.cs
@@ -4,121 +4,52 @@ using UnityEngine;
 
 public class coinTest : MonoBehaviour
 {
-    Animator anim;  //animation component of the coin
-    Material mat;   //material component of the coin
-
-    bool hasBeenFlipped = false;    //quick check to see if this coin has been flipped yet
-
-    bool hasFadedOut = false;   //redundant bool to check if the coin has finished fading out, only use for reset function
-
-    float fadeSpeed = 1f;   //how quickly the coin fades in or out
-    float fadeDelay = 1f;   //delay after the coin lands before we begin to fade out
+    Animator anim;
+    Material mat;
 
     private void Start()
     {
-        //get the components we need to work with
-
         anim = GetComponent<Animator>();
         mat = GetComponent<Renderer>().material;
     }
 
-    /*private void Update()
+    public IEnumerator BeginFlipSequence(bool _isAtk)
     {
-        //press space to begin fading in the coin
-
-        if (Input.GetKeyDown(KeyCode.Space) && !hasBeenFlipped)
-        {
-            StartCoroutine(FadeCoin(true));
-        }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ResetCoin();
-        }
-    }*/
-
-    public void BeginFlipSequence(bool _isAtk)
-    {
-        StartCoroutine(FadeInCoin(_isAtk));
-    }
-
-    IEnumerator FadeInCoin(bool _isAtk)
-    {
-        Color newColor = mat.color;
-
-        while (newColor.a < 1)
-        {
-            newColor.a += (fadeSpeed * Time.fixedDeltaTime);
-            if (newColor.a > 1)
-                newColor.a = 1;
-
-            mat.color = newColor;
-
-            yield return new WaitForFixedUpdate();
-        }
-
+        FadeCoin(true);
+        yield return new WaitForSeconds(0.25f);
         FlipCoin(_isAtk);
-    }
 
-    IEnumerator FadeOutCoin()
-    {
-        yield return new WaitForSeconds(fadeDelay);
+        yield return new WaitForSeconds(2.75f);
 
-        //Get color of the material on coin
-        Color newColor = mat.color;
-        
-        while (newColor.a > 0)
-        {
-            newColor.a -= (fadeSpeed * Time.fixedDeltaTime);
-            if (newColor.a < 0)
-                newColor.a = 0;
-
-            mat.color = newColor;
-
-            yield return new WaitForFixedUpdate();
-        }
+        FadeCoin(false);
 
         yield return null;
     }
 
-    void FlipCoin(bool _isAtk)
+    private void FadeCoin(bool _in)
     {
-        //0 - 1 defend and 1.000001 - 2 is attack or whatever floating point
-        //Remove this and use preferred RNG
+        string fade = (_in) ? "in" : "out";
+        Debug.Log("Fading " + fade);
+
         string animationName;
 
-        /*int rng = UnityEngine.Random.Range(1, 100);
-        Debug.Log(rng);
-
-        if (rng > 51)
-            animationName = "CoinAttack";
+        if (_in)
+            animationName = "CoinFadeIn";
         else
-            animationName = "CoinDefend";
-        */
+            animationName = "CoinFadeOut";
+
+        anim.Play(animationName, 0);
+    }
+
+    void FlipCoin(bool _isAtk)
+    {
+        string animationName;
 
         if (_isAtk)
             animationName = "CoinAttack";
         else
             animationName = "CoinDefend";
 
-        anim.Play(animationName);
-    }
-
-    void ResetCoin()
-    {
-        if (!hasFadedOut)
-            return;
-
-        hasBeenFlipped = false;
-    }
-
-    public void OnAnimationComplete()
-    {
-        //callback for when the animation finishes
-        //this script must be placed on the coin itself
-        //for this callback to work
-        //insert extra code here for any callback functionalities required
-
-        StartCoroutine(FadeOutCoin());
+        anim.Play(animationName, 1);
     }
 }
