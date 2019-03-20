7af616ac175022f05db14827585fbe6a7d2d7962
Daniel-PC\Daniel <rwa.ezimez@gmail.com>
2019-03-20 20:31:41 +1100

    +Map Update
    -New map model with sculpted geo
    -New Textures and materials
    
    -New props
    
    -See inside unit_view.unity scene

---
+++ Assets/_Scripts/Test Scripts/camera_dan.cs
@@ -0,0 +1,38 @@
+﻿namespace Testing
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class camera_dan : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+
+        private void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            if (Input.GetKey(KeyCode.W))
+                MoveCamera(Vector3.forward);
+
+            if (Input.GetKey(KeyCode.S))
+                MoveCamera(Vector3.back);
+
+            if (Input.GetKey(KeyCode.A))
+                MoveCamera(Vector3.left);
+
+            if (Input.GetKey(KeyCode.D))
+                MoveCamera(Vector3.right);
+        }
+
+        private void MoveCamera(Vector3 _direction)
+        {
+            transform.position += _direction * moveSpeed * Time.deltaTime;
+        }
+    }
+
+}
